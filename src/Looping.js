18a9dc379ac36a5ae0316c5dbcef2cb16b7bea66
ryan woods <federer5050@gmail.com>
2023-02-01 13:17:36 +0000

    playing with react

--- src/Looping.js
+++ src/Looping.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import MyArray from './MyArray.js'
+import { useState } from "react";
+
+
 
 const Looping = () => {
+
+        const handleTheClick = (e) => {
+            setColor("yellow")
+
+            if (e.target.className === 'steve') {
+                console.log('its steve!');
+            }
+        }
+
+   const [color, setColor] = useState('green')
     return (
         <div>
            {MyArray.map((obj) => {
                return (
+                <div key={obj.id}>
                     <h2>{obj.name}</h2>
+                    <button className={obj.name}  onClick={handleTheClick}>CLICK ME</button>
+                </div>
                )
            })}
         </div>
--- src/MyArray.js
+++ src/MyArray.js
@@ -1,8 +1,8 @@
 const  MyArray = [
-      {name: 'paul'},
-      {name: 'steve'},
-      {name: 'ryan'},
-      {name: 'tim'}
+      {name: 'paul', id: 0, age: 5},
+      {name: 'steve', id: 1, age: 3},
+      {name: 'ryan', id: 2, age: 7},
+      {name: 'tim', id: 8, age: 9}
 ]
 
 export default MyArray;

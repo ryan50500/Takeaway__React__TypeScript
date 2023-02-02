8a1a1ad775726ac811b9b9874bcafc6e5cd25c34
ryan woods <federer5050@gmail.com>
2023-02-02 18:04:26 +0000

    style components with class conditions

--- src/Modal.js
+++ src/Modal.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import ModalData from './ModalData.js'
+import styles from './Modal.module.css'
 
 const Modal = ({modal, index}) => {
     return (
-        <div className={modal ? 'modal visible' : "modal invisible"}>
-                <div className="close__modal">
-                    <div className="style__modal">
-                        <div className="style__modal__cross"></div>
+        <div className={styles.modal + ' ' + (modal ? styles.visible : '')}>
+                <div className={styles.close__modal}>
+                    <div className={styles.style__modal}>
+                        <div className={styles.style__modal__cross}></div>
                     </div>
                 </div>
-                <div className="modal__info">
+                <div className={styles.modal__info}>
                     <h2>{ModalData[index].name}</h2>
                     <h2>{ModalData[index].description}</h2>
                 </div>

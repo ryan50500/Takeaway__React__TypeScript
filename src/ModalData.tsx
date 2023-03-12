7bfb1871e1374a8a51038d33acb8c5563641a77d
ryan woods <federer5050@gmail.com>
2023-03-12 21:54:20 +0000

    updating bread price

--- src/ModalData.tsx
+++ src/ModalData.tsx
@@ -5,7 +5,7 @@ const  ModalData = [
       {name: 'Curry', description: 'A nice creamy Curry', price: 6.75, image: "https://ryan50500.github.io/takeaway/images/CURRY2.jpeg"},
       {name: 'Rogan', description: 'A nice creamy Rogan', price: 7.50, image: "https://ryan50500.github.io/takeaway/images/ROGAN2.jpeg"},
       {name: 'Tikka', description: 'A nice creamy Tikka', price: 9.50, image: "https://ryan50500.github.io/takeaway/images/TIKKA2.jpeg"},
-      {name: 'Naan', description: 'A nice creamy Naan', price: 6.50, image: "https://ryan50500.github.io/takeaway/images/NANN2.jpeg"},
+      {name: 'Naan', description: 'A nice creamy Naan', price: 2.75, image: "https://ryan50500.github.io/takeaway/images/NANN2.jpeg"},
       {name: 'Keema', description: 'A nice creamy Keema', price: 8.50, image: "https://ryan50500.github.io/takeaway/images/KEEMA2.jpeg"},
       {name: 'Bhaji', description: 'A nice creamy Bhaji', price: 10.50, image: "https://ryan50500.github.io/takeaway/images/BHAJI2.jpeg"}
 ]

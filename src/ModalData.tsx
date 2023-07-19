b6fd5984adcdd8b5f7ec0618cbfc482f55847350
Ryan Woods <38187920+ryan50500@users.noreply.github.com>
2023-07-19 21:21:11 +0100

    Update ModalData.tsx

--- src/ModalData.tsx
+++ src/ModalData.tsx
@@ -1,13 +1,13 @@
-const  ModalData = [
-      {name: 'Dansak', description: 'A nice creamy Dansak', price: 6.50, image: "https://ryan50500.github.io/takeaway/images/DANSAK2.jpeg"},
-      {name: 'Korma', description: 'A nice creamy Korma', price: 7.50, image: "https://ryan50500.github.io/takeaway/images/KORMA2.jpeg"},
-      {name: 'Biryani', description: 'A nice creamy Biryani', price: 8.50, image: "https://ryan50500.github.io/takeaway/images/BIRYANI2.jpeg"},
-      {name: 'Curry', description: 'A nice creamy Curry', price: 6.75, image: "https://ryan50500.github.io/takeaway/images/CURRY2.jpeg"},
-      {name: 'Rogan', description: 'A nice creamy Rogan', price: 7.50, image: "https://ryan50500.github.io/takeaway/images/ROGAN2.jpeg"},
-      {name: 'Tikka', description: 'A nice creamy Tikka', price: 9.50, image: "https://ryan50500.github.io/takeaway/images/TIKKA2.jpeg"},
-      {name: 'Naan', description: 'A nice creamy Naan', price: 2.75, image: "https://ryan50500.github.io/takeaway/images/NANN2.jpeg"},
-      {name: 'Keema', description: 'A nice creamy Keema', price: 8.50, image: "https://ryan50500.github.io/takeaway/images/KEEMA2.jpeg"},
-      {name: 'Bhaji', description: 'A nice creamy Bhaji', price: 3.75, image: "https://ryan50500.github.io/takeaway/images/BHAJI2.jpeg"}
+const ModalData = [
+      { name: 'Dansak', description: 'A nice creamy Dansak', price: 6.50, image: "https://ryan50500.github.io/takeaway__modal__popup/images/DANSAK2.jpeg" },
+      { name: 'Korma', description: 'A nice creamy Korma', price: 7.50, image: "https://ryan50500.github.io/takeaway__modal__popup/images/KORMA2.jpeg" },
+      { name: 'Biryani', description: 'A nice creamy Biryani', price: 8.50, image: "https://ryan50500.github.io/takeaway__modal__popup/images/BIRYANI2.jpeg" },
+      { name: 'Curry', description: 'A nice creamy Curry', price: 6.75, image: "https://ryan50500.github.io/takeaway__modal__popup/images/CURRY2.jpeg" },
+      { name: 'Rogan', description: 'A nice creamy Rogan', price: 7.50, image: "https://ryan50500.github.io/takeaway__modal__popup/images/ROGAN2.jpeg" },
+      { name: 'Tikka', description: 'A nice creamy Tikka', price: 9.50, image: "https://ryan50500.github.io/takeaway__modal__popup/images/TIKKA2.jpeg" },
+      { name: 'Naan', description: 'A nice creamy Naan', price: 2.75, image: "https://ryan50500.github.io/takeaway__modal__popup/images/NANN2.jpeg" },
+      { name: 'Keema', description: 'A nice creamy Keema', price: 8.50, image: "https://ryan50500.github.io/takeaway__modal__popup/images/KEEMA2.jpeg" },
+      { name: 'Bhaji', description: 'A nice creamy Bhaji', price: 3.75, image: "https://ryan50500.github.io/takeaway__modal__popup/images/BHAJI2.jpeg" }
 ]
 
 export default ModalData;

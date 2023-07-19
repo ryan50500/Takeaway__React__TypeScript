66b87b887fca767b4ef8db6f5c692b1e09f5b4c8
Ryan Woods <38187920+ryan50500@users.noreply.github.com>
2023-07-19 21:00:56 +0100

    Update Images.tsx

--- src/Images.tsx
+++ src/Images.tsx
@@ -1,14 +1,14 @@
 
 const Images = [
-    {image: 'https://ryan50500.github.io/takeaway/images/dansak.jpg', dishName: 'dansak', id: 0},
-    {image: 'https://ryan50500.github.io/takeaway/images/korma.jpg', dishName: 'korma', id: 1},
-    {image: 'https://ryan50500.github.io/takeaway/images/Biryani.jpg', dishName: 'biryani', id: 2},
-    {image: 'https://ryan50500.github.io/takeaway/images/curry.jpg', dishName: 'curry', id: 3},
-    {image: 'https://ryan50500.github.io/takeaway/images/rogan.jpg', dishName: 'rogan', id: 4},
-    {image: 'https://ryan50500.github.io/takeaway/images/tikka.jpg', dishName: 'tikka', id: 5},
-    {image: 'https://ryan50500.github.io/takeaway/images/nann.jpg', dishName: 'nann', id: 6},
-    {image: 'https://ryan50500.github.io/takeaway/images/keema.jpg', dishName: 'keema', id: 7},
-    {image: 'https://ryan50500.github.io/takeaway/images/bhaji.jpg', dishName: 'bhaji', id: 8},
+    {image: 'https://ryan50500.github.io/takeaway__modal__popup/images/dansak.jpg', dishName: 'dansak', id: 0},
+    {image: 'https://ryan50500.github.io/takeaway__modal__popup/images/korma.jpg', dishName: 'korma', id: 1},
+    {image: 'https://ryan50500.github.io/takeaway__modal__popup/images/Biryani.jpg', dishName: 'biryani', id: 2},
+    {image: 'https://ryan50500.github.io/takeaway__modal__popup/images/curry.jpg', dishName: 'curry', id: 3},
+    {image: 'https://ryan50500.github.io/takeaway__modal__popup/images/rogan.jpg', dishName: 'rogan', id: 4},
+    {image: 'https://ryan50500.github.io/takeaway__modal__popup/images/tikka.jpg', dishName: 'tikka', id: 5},
+    {image: 'https://ryan50500.github.io/takeaway__modal__popup/images/nann.jpg', dishName: 'nann', id: 6},
+    {image: 'https://ryan50500.github.io/takeaway__modal__popup/images/keema.jpg', dishName: 'keema', id: 7},
+    {image: 'https://ryan50500.github.io/takeaway__modal__popup/images/bhaji.jpg', dishName: 'bhaji', id: 8},
 ]
 
 export default Images
